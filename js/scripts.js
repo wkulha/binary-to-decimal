c6a876281ab9dbf58621ceda6a3fcad8926b9756
William Kulha <kulha.william@gmail.com>
2018-10-06 16:52:13 -0400

    Add all functionality for hexadecimal numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
+//Define error messages
 const binaryErrorMessage = 'Please enter a binary number (one that uses only 0 and 1)'
 const ternaryErrorMessage = 'Please enter a ternary number (one that uses only 0, 1, and 2)'
+const hexErrorMessage = 'Please enter a hexadecimal number (one that uses only 0-9 & a-f)'
 //Function to Check if a number is binary.
 let checkBinary = function(number) {
   let notBinary = /[^01]/;
@@ -22,7 +24,18 @@ let checkTernary = function(number) {
   }
 }
 
-//Function to convert Ternary Number to decimal number.
+//function to check if a number is a hexadecimal.
+let checkHex = function(number) {
+  let notHex = /[^0123456789abcdef]/;
+  if (notHex.test(number) === true) {
+    return true
+  }
+  else if (notHex.test(number) === false) {
+    return false
+  }
+}
+
+//Function to convert Binary Number to decimal number.
 let convertBinary = function(binaryNumber) {
   let binaryArray = Array.from(binaryNumber.toString()).map(Number);
   let arrayToAdd = []
@@ -52,11 +65,17 @@ let convertTernary = function(ternaryNumber) {
   return converted;
 }
 
+//Function to convert Hexadecimal Number into a decimal number_form
+let convertHex = function(hexNumber) {
+  let parsedForHex = parseInt(hexNumber, 16);
+  return parsedForHex;
+}
+
 //User logic
 $(document).ready(function() {
   $('#number_form').submit(function(event) {
     event.preventDefault();
-    let userInput = parseInt($('#user_input').val());
+    let userInput = ($('#user_input').val());
     let typeOfNumber = $("input:radio[name=typeradio]:checked").val();
     if (typeOfNumber === 'binary') {
       if (checkBinary(userInput) === true) {
@@ -73,6 +92,14 @@ $(document).ready(function() {
         $('.error_message').empty();
         let converted = convertTernary(userInput);
         $('.converter_output').text('The ternary number ' + userInput + ' is equal to ' + converted + ' as a decimal number.' )
+      }
+    } else if (typeOfNumber === 'hex') {
+      if(checkHex(userInput) === true) {
+        $('.error_message').text(hexErrorMessage);
+    } else {
+        $('.error_message').empty();
+        let converted = convertHex(userInput);
+        $('.converter_output').text('The hexadecimal number ' + userInput + ' is equal to ' + converted + ' as a decimal number.' )
       }
     }
   });
