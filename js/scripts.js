26dc7992082abe17e48a8ae50a411d1505a14a9b
William Kulha <kulha.william@gmail.com>
2018-10-06 11:34:39 -0400

    Add user logic to check whether the user input is a binary number, and if it is not, to display an error message on the form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
-let notBinary = /[^01]/;
+const errorMessage = 'Please enter a binary number (one that uses only 0 and 1)'
 
 let checkBinary = function(number) {
+  let notBinary = /[^01]/;
   if (notBinary.test(number) === true) {
     return true
   }
@@ -8,11 +9,16 @@ let checkBinary = function(number) {
     return false
   }
 }
+
 //User logic
 $(document).ready(function() {
   $('#binary_form').submit(function(event) {
     event.preventDefault();
     let userBinary = parseInt($('#user_input').val());
-    console.log(checkBinary(userBinary));
+    if (checkBinary(userBinary) === true) {
+      $('.error_message').text(errorMessage);
+    } else {
+      console.log(userBinary);
+    }
   });
 });
